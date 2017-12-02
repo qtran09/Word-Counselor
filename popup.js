0aaafe30295e9f95bc5035c08f4df15b2f16d1f9
shuyangw <shuyangw@umass.edu>
2017-12-02 04:20:34 -0500

    Right click on selected text functionality

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
-var evaluateEmotion = function(word){
-	var query = word.selectionText;
-	console.log("query");
-}
-
-chrome.contextMenus.create({
-	title: "Evaluate emotion",
-	contexts:["selection"],
-	onclick: evaluateEmotion
+chrome.runtime.onInstalled.addListener(function() {
+  var context = "selection";
+  var title = "Evaluate Emotion";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "context" + context});  
 });
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+function onClickHandler(info, tab) {
+  var sText = info.selectionText;
+  //Perform something with test
+};
