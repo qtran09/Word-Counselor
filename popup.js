f4c812fe96380c21dd6d1ff40cfd419b6220cd5f
shuyangw <shuyangw@umass.edu>
2017-12-02 06:04:52 -0500

    Cleanup of code, popup not complete yet

--- popup.js
+++ popup.js
@@ -1,13 +1,37 @@
 chrome.runtime.onInstalled.addListener(function() {
-  var context = "selection";
-  var title = "Evaluate Emotion";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});  
+	var context = "selection";
+	var title = "Evaluate Emotion";
+	var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});  
 });
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
+function prepareAndSendMessage(emotions){
+	var alertString = "We've detected these emotions in your selected text:\n";
+	for(x in emotions){
+		console.log(x + " " + emotions[x]);
+	}
+	console.log("args");
+}
+
 function onClickHandler(info, tab) {
-  var sText = info.selectionText;
-  //Perform something with test
+	var sText = info.selectionText;
+	//Perform something with test
+	$.post(
+		'https://apiv2.indico.io/emotion',
+		JSON.stringify({
+			'api_key': "2d3c91cb08abe04e37203439107e5ad6",
+			'data': sText,
+			'threshold': 0.1
+		})
+	).then(function(res) {
+		var emotionDict = {};
+		jsonObject = JSON.parse(res, (key, value) => {
+			if(typeof value === "number"){
+				emotionDict[key] = value;
+			}
+		});
+		prepareAndSendMessage(emotionDict);
+	});
 };
+
