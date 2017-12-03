06eed1f8c5adc671140069b32caacda74444fce4
shuyangw <shuyangw@umass.edu>
2017-12-03 03:22:25 -0500

    organize index.js

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function getSelectionText(){
     } else if (document.selection && document.selection.type != "Control"){
         text = document.selection.createRange().text;
     }
-    wordArr = text.replace(/[^a-zA-Z ]/g,'').split(" ").filter(function(n){ return n != "" });
+    wordArr = text.replace(/[^a-zA-Z ]/g,'').split(" ").filter(function(n){ 
+        return n != "" 
+    });
     return wordArr;
 }
 
@@ -25,25 +27,27 @@ function jQueryMain(){
                 url: 'https://wordsapiv1.p.mashape.com/words/'+Text+'/synonyms',
                 type: 'get',
                 headers: {
-'X-Mashape-Key': "RBGdikllvBmshXcwx3hyEE5gIwXjp1KJuxZjsnzcGMfpfOq2Vp",   //If your header name has spaces or any other char not appropriate
-'X-Mashape-Host': "wordsapiv1.p.mashape.com"  //for object property name, use quoted notation shown in second
-},
-success: function (data) {
-    console.info("data:")
-    console.info(data);
-    for (var i = 0; i < data.synonyms.length; i++) {
-        synList.push(data.synonyms[i]);
-    }
-    console.info("synlist:");
-    console.info(synList)
-},
-async: false,
-error: function (xhr, ajaxOptions, thrownError){
-    console.info("Error:"+Text);
-    console.info(thrownError);
-}
+                    //If your header name has spaces or any other char not appropriate
+                    'X-Mashape-Key': "RBGdikllvBmshXcwx3hyEE5gIwXjp1KJuxZjsnzcGMfpfOq2Vp",   
+                    //for object property name, use quoted notation shown in second
+                    'X-Mashape-Host': "wordsapiv1.p.mashape.com"  
+                },
+                success: function (data) {
+                    console.info("data:")
+                    console.info(data);
+                    for (var i = 0; i < data.synonyms.length; i++) {
+                        synList.push(data.synonyms[i]);
+                    }
+                    console.info("synlist:");
+                    console.info(synList)
+                },
+                async: false,
+                error: function (xhr, ajaxOptions, thrownError){
+                    console.info("Error:"+Text);
+                    console.info(thrownError);
+                }
 
-});
+            });
             if(typeof synList === 'undefined' || synList.length <= 0)
                 console.info("No synonyms found");
             return synList;
@@ -65,24 +69,23 @@ error: function (xhr, ajaxOptions, thrownError){
                     'api_key': "2d3c91cb08abe04e37203439107e5ad6",
                     'threshold': 0.1,
                     'data' : Text
-                }),
-// headers: {
-//     'api_key': "2d3c91cb08abe04e37203439107e5ad6",
-//     'threshold': 0.1
-// },
-success: function (data) {
-//console.info(data);
-jsono = JSON.parse(data);
-console.info(jsono);
-jsondict = jsono;
-
-},
-async: false
-});
-//console.log(maxEmotion[0]);
-return jsondict;
-}
-});
+            }),
+            // headers: {
+            //     'api_key': "2d3c91cb08abe04e37203439107e5ad6",
+            //     'threshold': 0.1
+            // },
+            success: function (data) {
+                //console.info(data);
+                jsono = JSON.parse(data);
+                console.info(jsono);
+                jsondict = jsono;
+            },
+            async: false
+            });
+            //console.log(maxEmotion[0]);
+            return jsondict;
+            }
+        });
 
     console.log("Search Thesaurus for better words");
     var Texta = getSelectionText();
@@ -109,16 +112,10 @@ return jsondict;
         return second[1] - first[1];
     });    
 
-// var relevantWords = [];
-// for(var i=0; i<sortedArr.length; i++){
-//   relevantWords.push(sortedArr[i][0]);
-// }
-// console.log(relevantWords);
-
-var recText = "";
-for(var i=0; i<sortedArr.length; i++){
-    recText += (sortedArr[i][0] + " ");
-}
-console.log(recText);
-document.getElementById("RecommendationText").innerHTML = recText;
+    var recText = "";
+    for(var i=0; i<sortedArr.length; i++){
+        recText += (sortedArr[i][0] + " ");
+    }
+    console.log(recText);
+    document.getElementById("RecommendationText").innerHTML = recText;
 }
